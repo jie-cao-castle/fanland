f68f34d3e7f5c07788967ffa8ebc13b2e0e5112a
jie-cao-castle <100085353+jie-cao-castle@users.noreply.github.com>
2022-02-22 17:49:07 +0800

    add product db and manager

---
+++ db/product_db.go
@@ -0,0 +1,57 @@
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"fanland/db/product_db"
+)
+
+type ProductDB struct {
+
+}
+
+func (f *ProductDB) init() error {
+	db, err := sql.Open("mysql",
+		"user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
+func (f *ProductDB) getById(id int64)(*Product) {
+	rows, err := db.Query("select id, name from users where id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (f *ProductDB) update(product *ProductDB) error {
+	stmt, err := db.Prepare("INSERT INTO users(name) VALUES(?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec("Dolly")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+}
---
+++ manager/product_manager.go
@@ -0,0 +1,4 @@
+type ProductManager struct {
+	productDb *ProductDB
+}
+
