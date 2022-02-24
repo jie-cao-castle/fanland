6716bca4cebc527e703be83ddd9bf32a44a11f53
jie-cao-castle <100085353+jie-cao-castle@users.noreply.github.com>
2022-02-24 14:28:08 +0800

    fix nft db build

--- db/nft_db.go
+++ db/nft_db.go
@@ -1,15 +1,16 @@
 package dao
+
 import (
 	"context"
 	"database/sql"
+	"fanland/db/dao"
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 	"time"
-	"fanland/db/dao"
 )
 
 type NftDB struct {
-	db *sql.DB;
+	db *sql.DB
 }
 
 func (f *NftDB) init() error {
@@ -23,24 +24,24 @@ func (f *NftDB) init() error {
 	return nil
 }
 
-func (f *NftDB) GetById(id uint64)(nftDO *dao.NftDO, err error) {
+func (f *NftDB) GetById(id uint64) (nftDO *dao.NftDO, err error) {
 	var (
-		nftId uint64
-		productId uint64
+		nftId       uint64
+		productId   uint64
 		productName string
-		chainId uint64
-		chainCode string
-		chainName string
+		chainId     uint64
+		chainCode   string
+		chainName   string
 		tokenSymbol string
-		tokenName string
-		price uint64
-		priceUnit uint64
-		createTime time.Time
-		updateTime time.Time
+		tokenName   string
+		price       uint64
+		priceUnit   uint64
+		createTime  time.Time
+		updateTime  time.Time
 	)
 
 	rows, err := f.db.Query("select id, product_id, prodect_name, chain_id, chain_code, chain_name, token_symbol, token_name, price, price_unit, create_time, update_time from nft where id = ?", id)
-	
+
 	if err != nil {
 		return nil, err
 	}
@@ -61,42 +62,45 @@ func (f *NftDB) GetById(id uint64)(nftDO *dao.NftDO, err error) {
 	}
 
 	nftObj := &dao.NftDO{
-		Id: nftId,
-		ProductId: productId,
-		productName string
-		chainId int64
-		chainCode string
-		chainName string
-		tokenSymbol string
-		tokenName string
-		price int64
-		priceUnit int64
-		createTime time.Time
-		updateTime time.Time
+		Id:          nftId,
+		ProductId:   productId,
+		ProductName: productName,
+		ChainId:     chainId,
+		ChainCode:   chainCode,
+		ChainName:   chainName,
+		TokenSymbol: tokenSymbol,
+		TokenName:   tokenName,
+		Price:       price,
+		PriceUnit:   priceUnit,
+		CreateTime:  createTime,
+		UpdateTime:  updateTime,
 	}
 	return nftObj, nil
 }
 
-func (f *NftDB) insert(nft *dao.NftDO)(err error) {
+func (f *NftDB) insert(nft *dao.NftDO) (err error) {
 
-	query := "INSERT INTO product(product_id, prodect_name, chain_id, chain_code, chain_name, token_symbol, token_name, price, price_unit, create_time, update_time) VALUES (?, ?, ? ,?, ? , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)"
-    ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancelfunc()
-    stmt, err := f.db.PrepareContext(ctx, query)
+	query := "INSERT INTO nft(product_id, prodect_name, chain_id, chain_code, chain_name, token_symbol, token_name, " +
+		"price, price_unit, create_time, update_time) " +
+		"VALUES (?, ?, ? ,?, ? ,?, ?, ? ,?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	stmt, err := f.db.PrepareContext(ctx, query)
 
-    if err != nil {
-    	return err
-    }
-    defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-	res, err := stmt.ExecContext(ctx, nft.name, nft.desc, nft.imgUrl, nft.nft_id, nft.tag_ids)
-	if err != nil {  
+	res, err := stmt.ExecContext(ctx, nft.ProductId, nft.ProductName, nft.ChainId, nft.ChainCode, nft.ChainCode,
+		nft.TokenSymbol, nft.TokenName, nft.Price, nft.PriceUnit)
+	if err != nil {
 		log.Printf("Error %s when inserting row into products table", err)
 		return err
 	}
 
 	_, err = res.RowsAffected()
-	if err != nil {  
+	if err != nil {
 		log.Printf("Error %s when finding rows affected", err)
 		return err
 	}
@@ -104,18 +108,15 @@ func (f *NftDB) insert(nft *dao.NftDO)(err error) {
 	return nil
 }
 
-
-func (f *NftDB) update(product *db.ProductDAO) error {
-	insForm, err := db.Prepare("UPDATE Prodect SET name=?, city=? WHERE id=?")
-
-	query := "UPDATE Prodect SET product_name=?, desc=?, tag_ids = ?, update_time = CURRENT_TIMESTAMP WHERE id=?"
-    ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+func (f *NftDB) update(nft *dao.NftDO) error {
+	query := "UPDATE Prodect SET price =?, priceUnit=?, update_time = CURRENT_TIMESTAMP WHERE id=?"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
-	stmt, err := db.PrepareContext(ctx, query)
+	stmt, err := f.db.PrepareContext(ctx, query)
 	if err != nil {
 		log.Fatal(err)
 	}
-	res, err := stmt.ExecContext(ctx, product.name, product.desc, product.tag_ids, id)
+	res, err := stmt.ExecContext(ctx, nft.Price, nft.PriceUnit, nft.Id)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -125,58 +126,64 @@ func (f *NftDB) update(product *db.ProductDAO) error {
 		log.Fatal(err)
 	}
 
+	if rowCnt != 1 {
+		log.Infof("no update")
+	}
+
 	return err
 }
 
-
-func (f *NftDB) getList(limit int64, offset int64)(products []*db.ProductDAO, err error) {
+func (f *NftDB) getList(limit int64, offset int64) ([]*dao.NftDO, error) {
 	var (
-		id int64
-		productId int64
+		nftId       uint64
+		productId   uint64
 		productName string
-		chainId int64
-		chainCode string
-		chainName string
+		chainId     uint64
+		chainCode   string
+		chainName   string
 		tokenSymbol string
-		tokenName string
-		price int64
-		priceUnit int64
-		createTime time.Time
-		updateTime time.Time
+		tokenName   string
+		price       uint64
+		priceUnit   uint64
+		createTime  time.Time
+		updateTime  time.Time
 	)
-	rows, err := db.Query("select id, product_name, desc, imgUrl, nft_id, tag_ids, create_time, update_time from product LIMIT ? OFFSET ? ", limit, offset)
-	
+	rows, err := f.db.Query("select id, nft_id, product_id, product_name, chain_id, chain_code, chain_name, "+
+		"token_symbol, tokenName, price, priceUnit, create_time, update_time from nft LIMIT ? OFFSET ? ", limit, offset)
+
 	if err != nil {
-		return (nil, err)
+		return nil, err
 	}
 
 	defer rows.Close()
-	nfts = []
+	var nfts []*dao.NftDO
 	for rows.Next() {
-		err := rows.Scan(&id, &name, &desc, &imgUrl, &nftId, &tags, &createTime, &updateTime)
+		err := rows.Scan(&nftId, &productId, &productName, &chainId, &chainCode, &chainName, &tokenSymbol, &tokenName,
+			&price, &priceUnit, &createTime, &updateTime)
 		if err != nil {
-			return (nil, err)
+			return nil, err
 		}
 
-		nft := &db.NftDAO{
-			id : id
-			productId : productId
-			productName : productName
-			chainId : chainId
-			chainCode : chainCode
-			chainName : chainName
-			tokenSymbol : tokenSymbol
-			tokenName : tokenName
-			price : price
-			priceUnit : priceUnit
-			createTime : createTime
-			updateTime :updateTime
+		nftObj := &dao.NftDO{
+			Id:          nftId,
+			ProductId:   productId,
+			ProductName: productName,
+			ChainId:     chainId,
+			ChainCode:   chainCode,
+			ChainName:   chainName,
+			TokenSymbol: tokenSymbol,
+			TokenName:   tokenName,
+			Price:       price,
+			PriceUnit:   priceUnit,
+			CreateTime:  createTime,
+			UpdateTime:  updateTime,
 		}
+		nfts = append(nfts, nftObj)
 	}
 
 	err = rows.Err()
 	if err != nil {
-		return ([], err)
+		return nil, err
 	}
-	return (nfts, nil)
+	return nfts, nil
 }
